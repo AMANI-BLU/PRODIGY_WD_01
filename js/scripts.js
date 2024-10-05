f0e4717fb73aacb8b4f83fb2d1f924338d2fb802
AMANI-BLU <solomonamanuel66@gmail.com>
2024-10-05 21:31:00 +0300

    done

--- js/scripts.js
+++ js/scripts.js
@@ -19,33 +19,28 @@ const menuIcon = document.getElementById('menu-icon');
 const navbar = document.querySelector('.navbar');
 const header = document.querySelector('header');
 
-// Toggle the navbar on menu icon click
 menuIcon.addEventListener('click', () => {
-    navbar.classList.toggle('active'); // Toggle the 'active' class
+    navbar.classList.toggle('active');
 });
 
-// Change navbar color on scroll
 window.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
-        header.classList.add('header-scrolled'); // Add class on scroll
+        header.classList.add('header-scrolled'); 
     } else {
-        header.classList.remove('header-scrolled'); // Remove class when back at top
+        header.classList.remove('header-scrolled'); 
     }
 });
 
 
- // Get all navigation links
     const navLinks = document.querySelectorAll('.navbar a');
     const footerLinks = document.querySelectorAll('.footer-links a');
 
-    // Function to set the active link
     function setActiveLink(link) {
-        navLinks.forEach(link => link.classList.remove('active')); // Remove active from all
-        footerLinks.forEach(link => link.classList.remove('active')); // Remove active from all
-        link.classList.add('active'); // Add active to the clicked link
+        navLinks.forEach(link => link.classList.remove('active')); 
+        footerLinks.forEach(link => link.classList.remove('active')); 
+        link.classList.add('active');
     }
 
-    // Event listener for nav links
     navLinks.forEach(link => {
         link.addEventListener('click', function() {
             setActiveLink(this);
