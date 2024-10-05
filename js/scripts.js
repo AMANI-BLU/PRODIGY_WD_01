4e07054e5bc4c986a7295cdc70c22b6cd52aec7a
AMANI-BLU <solomonamanuel66@gmail.com>
2024-10-05 17:57:05 +0300

    homepage design

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,34 @@
- var swiper = new Swiper(".home", {
-      spaceBetween: 30,
-      centeredSlides: true,
-      autoplay: {
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
         delay: 2500,
         disableOnInteraction: false,
-      },
-      pagination: {
+    },
+    pagination: {
         el: ".swiper-pagination",
         clickable: true,
-      },
-      navigation: {
+    },
+    navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
-      },
-    });
+    },
+});
+
+const menuIcon = document.getElementById('menu-icon');
+const navbar = document.querySelector('.navbar');
+const header = document.querySelector('header');
+
+// Toggle the navbar on menu icon click
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('active'); // Toggle the 'active' class
+});
+
+// Change navbar color on scroll
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        header.classList.add('header-scrolled'); // Add class on scroll
+    } else {
+        header.classList.remove('header-scrolled'); // Remove class when back at top
+    }
+});
